669eaaa5f27bc654968ebf858c530673a65ae241
Alessandro Nazzani <alessandro.nazzani@plurimedia.it>
2015-12-09 01:11:19 +0100

    Attacks

--- client/js/ctrl/torn.js
+++ client/js/ctrl/torn.js
@@ -31,51 +31,61 @@ function ($rootScope, $scope, $filter, $http, $cacheFactory, $location, $routePa
             $scope.faction_id= data._id;
          });
 
-      $http({
-         url: '/api/torn/list',
-         cache: true,
-         method: "GET",
-         params: { selections: $location.path().substring(1) }
-      }).success(function(items)
-      {
-         if (items.donationnews) items.donationnews= json2array(items.donationnews);
-
-         if (items.attacks)
-         {
-            items.attacks= json2array(items.attacks);
-            _.each(items.attacks, function(m)
-                                  {
-                                     if (m.respect_gain)
-                                        m.respect_gain= parseFloat(m.respect_gain);
-                                     if (m.attacker_faction)
-                                        m.attacker_faction= parseInt(m.attacker_faction);
-                                  });
-
-console.log(1,items.attacks.length);
-            items.our_attacks= _.where(items.attacks, { attacker_faction: $scope.faction_id });
-console.log(2,items.our_attacks.length);
-            items.defends= _.difference(items.attacks, items.our_attacks);
-console.log(3,items.defends.length);
-            /*items.defends= { id: 'd',
-                             collection: _.filter(items.attacks, function(a)
-                                         {
-                                            return a.attacker_faction != $scope.faction_id;
-                                         })
-                           };
-
-            items.attacks= { id: 'a', collection: _.difference(items.attacks, items.defends) };*/
-         }
-
-         if (items.members)
+      if (!$scope.faction_members)
+         $http({
+            url: '/api/torn/list',
+            cache: true,
+            method: "GET",
+            params: { selections: 'basic' }
+         }).success(function(items)
          {
             items.members= json2array(items.members);
             _.each(items.members, function(m) { m.days_in_faction= parseInt(m.days_in_faction); });
             $scope.leader= _.findWhere(items.members, { _id: parseInt(items.leader) });
             $scope.coleader= _.findWhere(items.members, { _id: parseInt(items['co-leader']) });
-         }
+            $scope.faction_members= items.members;
+         });
 
-         $scope.items= items;
-      });
+      if ($location.path().substring(1) !== 'basic')
+         $http({
+            url: '/api/torn/list',
+            cache: true,
+            method: "GET",
+            params: { selections: $location.path().substring(1) }
+         }).success(function(items)
+         {
+            if (items.donationnews) items.donationnews= json2array(items.donationnews);
+
+            if (items.attacks)
+            {
+               items.attacks= json2array(items.attacks);
+               _.each(items.attacks, function(m)
+                                     {
+                                        if (m.respect_gain)
+                                           m.respect_gain= parseFloat(m.respect_gain);
+                                        if (m.attacker_faction)
+                                           m.attacker_faction= parseInt(m.attacker_faction);
+                                        if (m.attacker_id)
+                                           m.attacker_id= parseInt(m.attacker_id);
+                                        if (m.defender_id)
+                                           m.defender_id= parseInt(m.defender_id);
+                                     });
+
+               items.our_attacks= _.where(items.attacks, { attacker_faction: $scope.faction_id });
+               items.defends= _.difference(items.attacks, items.our_attacks);
+
+               _.each(items.our_attacks, function(a)
+               {
+                  a.attacker_name= _.findWhere($scope.faction_members, { _id: a.attacker_id }).name;
+               });
+               _.each(items.defends, function(d)
+               {
+                  d.defender_name= _.findWhere($scope.faction_members, { _id: d.defender_id }).name;
+               });
+            }
+
+            $scope.items= items;
+         });
    };
 
    $scope.order= function(collection, predicate)
