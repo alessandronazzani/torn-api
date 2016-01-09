392774220f1a3835971f97e7834aeaf7c3894ea7
Alessandro Nazzani <alessandro.nazzani@plurimedia.it>
2016-01-09 11:41:03 +0100

    Attacks

--- client/js/ctrl/torn.js
+++ client/js/ctrl/torn.js
@@ -31,7 +31,7 @@ function ($rootScope, $scope, $filter, $http, $cacheFactory, $location, $routePa
             $scope.faction_id= data._id;
          });
 
-      if (!$scope.faction_members)
+      //if (!$scope.items || !$scope.items.members)
          $http({
             url: '/api/torn/list',
             cache: true,
@@ -40,10 +40,9 @@ function ($rootScope, $scope, $filter, $http, $cacheFactory, $location, $routePa
          }).success(function(items)
          {
             items.members= json2array(items.members);
-            _.each(items.members, function(m) { m.days_in_faction= parseInt(m.days_in_faction); });
             $scope.leader= _.findWhere(items.members, { _id: parseInt(items.leader) });
             $scope.coleader= _.findWhere(items.members, { _id: parseInt(items['co-leader']) });
-            $scope.faction_members= items.members;
+            $scope.items= items;
          });
 
       if ($location.path().substring(1) !== 'basic')
@@ -61,14 +60,10 @@ function ($rootScope, $scope, $filter, $http, $cacheFactory, $location, $routePa
                items.attacks= json2array(items.attacks);
                _.each(items.attacks, function(m)
                                      {
-                                        if (m.respect_gain)
-                                           m.respect_gain= parseFloat(m.respect_gain);
-                                        if (m.attacker_faction)
-                                           m.attacker_faction= parseInt(m.attacker_faction);
-                                        if (m.attacker_id)
-                                           m.attacker_id= parseInt(m.attacker_id);
-                                        if (m.defender_id)
-                                           m.defender_id= parseInt(m.defender_id);
+                                        if (m.attacker_faction && m.attacker_faction == 0)
+                                           delete m.attacker_faction;
+                                        if (m.defender_faction && m.defender_faction == 0)
+                                           delete m.defender_faction;
                                      });
 
                items.our_attacks= _.where(items.attacks, { attacker_faction: $scope.faction_id });
@@ -76,15 +71,39 @@ function ($rootScope, $scope, $filter, $http, $cacheFactory, $location, $routePa
 
                _.each(items.our_attacks, function(a)
                {
-                  a.attacker_name= _.findWhere($scope.faction_members, { _id: a.attacker_id }).name;
+                  var attacker= _.findWhere($scope.items.members, { _id: a.attacker_id });
+                  if (attacker)
+                     a.attacker_name= attacker.name;
+                  else
+                     a.attacker_name= a.attacker_id;
                });
+
                _.each(items.defends, function(d)
                {
-                  d.defender_name= _.findWhere($scope.faction_members, { _id: d.defender_id }).name;
+                  var defender= _.findWhere($scope.items.members, { _id: d.defender_id });
+                  if (defender)
+                     d.defender_name= defender.name;
+                  else
+                     d.defender_name= d.attacker_id;
                });
+
+               var people= _.groupBy(items.our_attacks, 'attacker_name');
+               items.hof= _.map(people, function(g, key)
+               {
+                  return { attacker_name: key, 
+                           respect_gain: parseFloat(_.reduce(g, function(m, x)
+                                         {
+                                            var i= parseFloat(m) + parseFloat(x.respect_gain);
+                                            return i.toFixed(5);
+                                         }, 0))
+                         };
+               });
+               items.hof= _.sortBy(items.hof, 'respect_gain').reverse();
             }
 
+            var members= $scope.items.members;
             $scope.items= items;
+            $scope.items.members= members;
          });
    };
 
